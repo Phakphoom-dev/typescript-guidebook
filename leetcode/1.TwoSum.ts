d48bc42e8229843da01c3eb927f8e823a07fbdd8
Phakphoom-dev <phakphoomartydev@gmail.com>
2024-03-07 11:20:49 +0700

    at leet code problem

---
+++ leetcode/1.TwoSum.ts
@@ -0,0 +1,30 @@
+type obj = {
+  [key: number]: number;
+};
+
+function twoSum(nums: number[], target: number): number[] {
+  const hMap: obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in hMap) {
+      return [hMap[target - nums[i]], i];
+    }
+    hMap[nums[i]] = i;
+  }
+  return [-1, -1];
+}
+
+// ---------------------------------------------------------------------------------------------
+
+var twoSum2 = function (nums: number[], target: number) {
+  let tmp = new Map();
+
+  for (let i = 0; i < nums.length; ++i) {
+    if (tmp.has(target - nums[i])) {
+      return [tmp.get(target - nums[i]), i];
+    }
+    tmp.set(nums[i], i);
+  }
+
+  return [-1, -1];
+};
